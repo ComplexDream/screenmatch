404ef73b0069ad46d00c4a496dd3e0733b70353b
ComplexDream <edulevis2014@gmail.com>
2025-01-03 17:47:11 -0500

    Practique el encapsulamiento

--- src/Principal.java
+++ src/Principal.java
@@ -1,21 +1,26 @@
+import com.aluracursos.screenmatch.modelos.Pelicula;
+
 public class Principal {
     public static void main(String[] args) {
         Pelicula miPelicula = new Pelicula();
-        miPelicula.nombre="Encanto";
-        miPelicula.fechaDeLanzamiento=2021;
-        miPelicula.duracionEnMinutos=120;
+        miPelicula.setNombre("Encanto");
+        miPelicula.setFechaDeLanzamiento(2021);
+        miPelicula.setDuracionEnMinutos(120);
+        miPelicula.setIncluidoEnElPlan(true);
 
-        System.out.println("mi pelicula es: "+miPelicula.nombre);
-        System.out.println("su fecha de lanzamiento es: " + miPelicula.fechaDeLanzamiento);
+        miPelicula.muestraFichaTecnica();
+        miPelicula.evalua(7.8);
+        miPelicula.evalua(10.0);
 
-        Pelicula otraPelicula = new Pelicula();
-        otraPelicula.nombre="matrix";
-        otraPelicula.fechaDeLanzamiento= 1998;
-        otraPelicula.duracionEnMinutos=180;
-        System.out.println("mi pelicula es: "+otraPelicula.nombre);
-        System.out.println("su fecha de lanzamiento es: " + otraPelicula.fechaDeLanzamiento);
+        System.out.println(miPelicula.getSumaDeLasEvaluaciones());
+        System.out.println(miPelicula.getTotalDeLasEvaluaciones());
+        System.out.println(miPelicula.calculaMedia());
 
-
+//        com.aluracursos.screenmatch.modelos.Pelicula otraPelicula = new com.aluracursos.screenmatch.modelos.Pelicula();
+//        otraPelicula.nombre="matrix";
+//        otraPelicula.fechaDeLanzamiento= 1998;
+//        otraPelicula.duracionEnMinutos=180;
+//        otraPelicula.muestraFichaTecnica();
 
 
     }
